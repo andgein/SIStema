3f37e2991fed625e7705e9f0591d3f6ebdabb393
Eugene Kurpilyansky <dembelz@yandex.ru>
2017-02-21 22:43:54 +0300

    delete decorators.school_view; add middleware.SchoolViewMiddleware

--- src/web/sistema/settings.py
+++ src/web/sistema/settings.py
@@ -85,6 +85,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+
+    'sistema.middleware.SchoolMiddleware'
 )
 
 ROOT_URLCONF = 'sistema.urls'
