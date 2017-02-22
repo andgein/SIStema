057ab84eb6cd1f26435fbbaa41af3940f8f5c55f
Eugene Kurpilyansky <dembelz@yandex.ru>
2017-02-22 21:04:05 +0300

    delete needless view 'entrance:initial.reset'

--- src/web/modules/entrance/urls.py
+++ src/web/modules/entrance/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     url(r'^check/(?P<group_name>[^/]+)/$', staff_views.check_group, name='check_group'),
     url(r'^solution/(?P<solution_id>\d+)/$', staff_views.solution, name='user_solution'),
 
-    url(r'^initial/reset/$', staff_views.initial_reset, name='initial.reset'),
     url(r'^initial/auto_reject/$', staff_views.initial_auto_reject, name='initial.auto_reject'),
     url(r'^initial/checking_groups/$', staff_views.initial_checking_groups, name='initial.checking_groups'),
     ]
