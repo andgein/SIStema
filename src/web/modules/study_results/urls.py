06bb451e664bf97841ad0e2100ee2e60ebff27ca
Irina Brovar <iris-arcus@yandex.ru>
2017-02-22 23:25:43 +0100

    Small url nits

--- src/web/modules/study_results/urls.py
+++ src/web/modules/study_results/urls.py
@@ -3,7 +3,6 @@ from modules.study_results.staff import views as staff_views
 
 urlpatterns = [
     # Staff urls
-    url(r'^$', staff_views.study_results, name='study_results'),
     url(r'^user/(?P<user_id>\d+)/$',
         staff_views.study_result_user, name='study_result_user'),
 ]
