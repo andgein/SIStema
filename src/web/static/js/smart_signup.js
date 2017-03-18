7dbd730fffabf78a350f2f735f7f375a06b50bcb
Eugene Kurpilyansky <dembelz@yandex.ru>
2017-03-18 23:53:37 +0300

    add more javascript code in smart_signup.js

--- src/web/static/js/smart_signup.js
+++ src/web/static/js/smart_signup.js
@@ -1,9 +1,78 @@
 $(document).ready(function () {
-    var form = $('form');
+    var $container = $('#similar_account_container');
+    var $accounts_data = $container.find('.accounts');
+    var url = $container.attr('data-url');
+    var $form = $('form');
+    var $signup_button = $('#signup_button');
+    var $decline_button = $('#decline_similar_accounts_button');
+    var $close_button = $('#close_similar_accounts');
+    var declined_accounts = {};
+    var showed_accounts = [];
+    var not_declined_count = 0;
+
+    $container.css({
+        position: "fixed",
+        marginLeft: 0, marginTop: 0,
+        top: 0, left: 0,
+        zIndex: 100,
+        backgroundColor: "orange"
+    });
+
+    function close_container() {
+       $container.addClass('closed');
+    }
+
+    function expand_container() {
+       $container.removeClass('closed');
+    }
+
+    $close_button.click(function() {
+       $container.toggleClass('closed');
+    });
+
+    $decline_button.click(function () {
+        for (var i = 0; i < showed_accounts.length; ++i) {
+            declined_accounts[showed_accounts[i]] = 1;
+        }
+        not_declined_count = 0;
+        $signup_button.removeAttr("disabled");
+        $decline_button.hide();
+        $close_button.show();
+        close_container();
+    });
+
+    function show_similar_accounts(data) {
+        not_declined_count = 0;
+        for (var account_id in data) {
+            if (!(account_id in declined_accounts)) {
+                not_declined_count++;
+            }
+        }
+        if (data.length == 0) {
+            $signup_button.removeAttr("disabled");
+            $container.hide();
+        } else {
+            showed_accounts = Object.keys(data);
+            $accounts_data.html(Object.values(data).join());
+            if (not_declined_count == 0) {
+                $signup_button.removeAttr("disabled");
+                $close_button.show();
+                $decline_button.hide();
+                close_container();
+            } else {
+                $signup_button.attr("disabled", true);
+                $close_button.hide();
+                $decline_button.show();
+                expand_container();
+            }
+            $container.show();
+        }
+    }
+
     function update_similar_accounts() {
-        $.post('/accounts/find-similar/', form.serializeArray())
+        $.post(url, $form.serializeArray())
             .done(function (data) {
-                console.log(data);
+                show_similar_accounts(data);
             })
             .fail(function () {
                 alert("error");
--- src/web/users/views.py
+++ src/web/users/views.py
@@ -3,6 +3,8 @@ from django import shortcuts
 from django.contrib.auth import decorators as auth_decorators
 from sistema import decorators as sistema_decorators
 from users import forms, models, search_utils
+from django.template.loader import render_to_string
+import django.http
 
 
 @auth_decorators.login_required()
@@ -10,12 +12,16 @@ def account_settings(request):
     return shortcuts.render(request, 'users/account_settings.html')
 
 
+def _render_user_account(user):
+    return render_to_string('account/_similar_account.html', {'user': user})
+
+
 @http_decorators.require_POST
 def find_similar_accounts(request):
     form = forms.UserProfileForm(data=request.POST)
     form.full_clean()
     users = search_utils.SimilarAccountSearcher(form.fill_user_profile(request)).search()
-    return shortcuts.render(request, 'account/_similar_accounts.html', {'users': users})
+    return django.http.JsonResponse({user.id: _render_user_account(user) for user in users})
 
 
 def _init_profile_form(request):
