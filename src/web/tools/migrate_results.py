1c6d1995066c0c3170f13659eff397ac3ffb4feb
Andrew Gein <andgein@yandex.ru>
2021-05-23 12:04:21 +0500

    Fix bug in migrate_results

--- src/web/tools/migrate_results.py
+++ src/web/tools/migrate_results.py
@@ -41,13 +41,13 @@ def preprocess_mark(mark):
 def get_parallel_from_group(group, school):
     parallel = group[0].lower()
     if parallel == 'c':
-        if len(group) > 1 and group[1] in '123':
+        if len(group) > 1 and group[1].isdigit() and group[1] in '123':
             parallel = 'c.python'
-        elif len(group) > 1 and group[1] in '56':
+        elif len(group) > 1 and group[1].isdigit() and group[1] in '56':
             parallel = 'c.cpp'
     if group.lower() == 'a0':
         parallel = group
-    if len(group) > 1 and int(group[1]) >= 7:
+    if len(group) > 1 and group[1].isdigit() and int(group[1]) >= 7:
         parallel += '_prime'
     print(parallel, end=' ', flush=True)
     return schools.models.Parallel.objects.get(short_name=parallel,
