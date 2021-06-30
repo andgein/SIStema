4938f37d701496bddfde72da16eeb45eb8e71635
Andrew Gein <andgein@yandex.ru>
2021-06-30 16:33:37 +0400

    Show enrolled-only entrance steps to non-approved users

--- src/web/modules/entrance/home/blocks.py
+++ src/web/modules/entrance/home/blocks.py
@@ -58,10 +58,13 @@ class EntranceStepsHomePageBlock(home.models.AbstractHomePageBlock):
             self.school,
             user
         )
-        if status is not None and status.is_enrolled and status.is_approved:
-            sessions_and_parallels = status.sessions_and_parallels.filter(
-                selected_by_user=True
-            )
+        if status is not None and status.is_enrolled:
+            if status.sessions_and_parallels.filter(selected_by_user=True).count() > 0:
+                sessions_and_parallels = status.sessions_and_parallels.filter(
+                    selected_by_user=True
+                )
+            else:
+                sessions_and_parallels = status.sessions_and_parallels.all()
 
             enrolled_to_session_ids = nested_query_list(
                 sessions_and_parallels.values_list('session_id', flat=True)
